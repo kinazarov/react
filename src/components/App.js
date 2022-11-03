f5a5d980ccdab6110241ead7b8286e20c7045b5c
kinazarov <kinazarov@gmail.com>
2022-11-03 12:00:57 +0200

    2.2

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import MusicList from './MusicList'
 import PostList from './PostList'
 import MyButton from './UI/MyButton/MyButton';
@@ -11,19 +11,26 @@ function App() {
         {id: 3, title: 'CSS', content:'it is not a proglang'}
     ];
 
+    const bodyInputRef = useRef();
     const [postTitle, setPostTitle] = useState('')
     const [postContent, setPostContent] = useState('')
+    
+
+
     const addNewPost = (e) => {
         console.log("◩◩◩◩◩◩ e", e);
         e.preventDefault();
-        console.log("◩◩◩◩◩◩ postTitle, postContent", postTitle, postContent);
+        console.log("◩◩◩◩◩◩ postTitle, postContent, bodyInputRef.current.value", postTitle, postContent, bodyInputRef.current.value);
     }
 
     return (
         <div className="App">
             <form>
                 <MyInput value={postTitle} onChange={e => {setPostTitle(e.target.value); console.log("◩◩◩◩◩◩ postTitle", postTitle);}} type='text' placeholder='Название поста'></MyInput>
-                <MyInput value={postContent} onChange={e => {setPostContent(e.target.value); console.log("◩◩◩◩◩◩ postContent", postContent);}} type='text' placeholder='Содержание поста'></MyInput>
+
+                <input ref={bodyInputRef} type="text"></input>
+
+                {/* <MyInput value={postContent} onChange={e => {setPostContent(e.target.value); console.log("◩◩◩◩◩◩ postContent", postContent);}} type='text' placeholder='Содержание поста'></MyInput> */}
                 <MyButton onClick={addNewPost}>Запостить</MyButton>
             </form>
             <PostList posts={testPosts} title="Список постов 1"/>
